9b7207706bb06486041aa49dfc8bc16c5a3134a0
Timilehin Arigbede <Harriemannie@gmail.com>
2021-12-09 18:21:44 +0100

    Display property on home page and create app layout

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
 
-      <Flex flexwrap="wrap">
+      <Flex flexWrap="wrap">
         {propertiesForRent.map((property) => (
           <Property property={property} key={property.id} />
         ))}
@@ -59,9 +59,11 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         linkName="/search?purpose-for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
-      {propertiesForSale.map((property) => (
-        <Property property={property} key={property.id} />
-      ))}
+      <Flex flexWrap="wrap">
+        {propertiesForSale.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
     </Box>
   );
 }
